5183e04c2049bf892730e62e9a6620a47693faed
Roman Boboshko <romanboboshko89@gmail.com>
2018-05-14 14:33:54 +0300

    Added connection to RethinkDB, changed timestamp from database

--- client/src/api.js
+++ client/src/api.js
@@ -3,6 +3,8 @@ import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
 
 function subscribeToTimer(cb) {
+    console.log('subscribeToTimer');
+    
     socket.on('timer', timestamp => cb(timestamp));
     socket.emit('subscribeToTimer', 1000);
 }
