c6ccb449e1295f221241d64c0e356e859693be6a
Roman Boboshko <romanboboshko89@gmail.com>
2018-05-14 14:34:44 +0300

    Changed server index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const io = require('socket.io')();
+const r = require('rethinkdb');
 
-io.on('connection', client => {
-    client.on('subscribeToTimer', interval => {
-        console.log(interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
+function createDrawing({ connection, name }) {
+    r.table('drawings')
+        .insert({
+            name,
+            timestamp: new Date()
+        })
+        .run(connection)
+        .then(() => console.log('created drawing - ', name));
+}
+
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'drawing_whiteboard'
+}).then(connection => {
+    io.on('connection', client => {
+        client.on('subscribeToTimer', interval => {
+            r.table('timers')
+                .changes()
+                .run(connection)
+                .then(cursor => {
+                    cursor.each((err, timerRow) => {
+                        client.emit('timer', timerRow.new_val.timestamp);
+                    });
+                });
+        });
     });
 });
 
